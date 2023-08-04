ff1eb7f2a1350bddff39032b8a60ede528543839
goyalkunjann <125953017+goyalkunjann@users.noreply.github.com>
2023-08-04 21:18:35 +0530

    Jim and the orders

---
+++ A39.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jimOrders' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts 2D_INTEGER_ARRAY orders as parameter.
+     */
+
+    public static List<Integer> jimOrders(List<List<Integer>> orders) {
+    List<Integer> ans = new ArrayList<>();
+        int[][] serveOrder = new int [orders.size()][3];
+        for(int i = 0; i < orders.size() ; i++){
+            serveOrder[i] = new int[]{ i+1, orders.get(i).get(0), orders.get(i).get(0) + orders.get(i).get(1) };
+        }
+        Arrays.sort(serveOrder,(a,b)-> a[2] == b[2] ? a[0] - b[0] : a[2] - b[2]);
+        for(int[] customer : serveOrder){
+            ans.add(customer[0]);
+        }
+        return ans;
+
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> orders = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                orders.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.jimOrders(orders);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
